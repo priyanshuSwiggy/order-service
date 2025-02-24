1a995d459be25b32e7c455ffeac4684d860cf0eb
Priyanshu <kumar.priyanshu_ftc@external.swiggy.in>
2025-02-24 08:38:41 +0530

    get all orders case handled

--- src/test/java/com/swiggy/order/controller/OrderControllerTest.java
+++ src/test/java/com/swiggy/order/controller/OrderControllerTest.java
@@ -2,6 +2,7 @@ package com.swiggy.order.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.swiggy.order.dto.OrderRequestDto;
+import com.swiggy.order.dto.OrderResponseDto;
 import com.swiggy.order.exceptions.GlobalExceptionHandler;
 import com.swiggy.order.service.OrderService;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,11 +12,16 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Collections;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -119,5 +125,39 @@ public class OrderControllerTest {
                         .content(objectMapper.writeValueAsString(orderRequestDto)))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().json("{\"orderLines\":\"Order lines cannot be null\"}"));
+    }
+
+    @Test
+    void testGetAllOrdersSuccessfully() throws Exception {
+        final List<OrderResponseDto> orders = List.of(
+                OrderResponseDto.builder().restaurantId(1L).customerId(1L).deliveryAddress("123 Main St").orderLines(Collections.emptyList()).build(),
+                OrderResponseDto.builder().restaurantId(2L).customerId(2L).deliveryAddress("456 Elm St").orderLines(Collections.emptyList()).build()
+        );
+        when(orderService.getAllOrders()).thenReturn(orders);
+
+        MvcResult mvcResult = mockMvc.perform(get(ORDERS_URL)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(orders)))
+                .andReturn();
+
+        String expectedResponse = objectMapper.writeValueAsString(orders);
+        String actualResponse = mvcResult.getResponse().getContentAsString();
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    void testGetAllOrdersWhenNoOrdersAreAvailable() throws Exception {
+        when(orderService.getAllOrders()).thenReturn(Collections.emptyList());
+
+        MvcResult mvcResult = mockMvc.perform(get(ORDERS_URL)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"))
+                .andReturn();
+
+        String expectedResponse = "[]";
+        String actualResponse = mvcResult.getResponse().getContentAsString();
+        assertEquals(expectedResponse, actualResponse);
     }
 }
