b826bb5e778040178128ef6b947249ea0714e7f1
Priyanshu <kumar.priyanshu_ftc@external.swiggy.in>
2025-03-05 15:09:34 +0530

    added swagger docs

--- src/main/java/com/swiggy/order/controller/OrderController.java
+++ src/main/java/com/swiggy/order/controller/OrderController.java
@@ -4,6 +4,13 @@ import com.swiggy.order.dto.OrderRequestDto;
 import com.swiggy.order.dto.OrderResponseDto;
 import com.swiggy.order.enums.OrderStatus;
 import com.swiggy.order.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,28 +21,66 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "OrderController", description = "APIs for managing orders")
 @RequestMapping("/users/{userId}/orders")
 public class OrderController {
 
     private final OrderService orderService;
 
+    @Operation(summary = "Create a new order",
+            description = "Create a new order for a specific user")
+    @Parameter(name = "orderRequestDto", description = "The DTO containing information for creating a new order")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Successfully created a new order",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = OrderRequestDto.class))}),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid input for creating an order")
+    })
     @PostMapping
     public ResponseEntity<String> createOrder(@Valid @PathVariable Long userId, @Valid @RequestBody OrderRequestDto orderRequestDto) {
         orderService.createOrder(userId, orderRequestDto);
         return new ResponseEntity<>("Order created successfully", HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Retrieve all orders",
+            description = "Retrieve all the orders for a specific user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Successfully retrieved the orders",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = OrderResponseDto.class))})
+    })
     @GetMapping
     public ResponseEntity<List<OrderResponseDto>> getAllOrders(@Valid @PathVariable Long userId) {
         List<OrderResponseDto> orders = orderService.getAllOrders(userId);
         return new ResponseEntity<>(orders, HttpStatus.OK);
     }
 
+    @Operation(summary = "Retrieve an order by ID",
+            description = "Retrieve a specific order by its ID for a specific user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Successfully retrieved the order",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = OrderResponseDto.class))}),
+            @ApiResponse(responseCode = "404",
+                    description = "Order not found")
+    })
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderResponseDto> getOrderById(@Valid @PathVariable Long userId, @Valid @PathVariable Long orderId) {
         return new ResponseEntity<>(orderService.getOrderById(userId, orderId), HttpStatus.OK);
     }
 
+    @Operation(summary = "Update order status",
+            description = "Update the status of a specific order for a specific user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Successfully updated the order status"),
+            @ApiResponse(responseCode = "404",
+                    description = "Order not found")
+    })
     @PutMapping("/{orderId}/status")
     public ResponseEntity<String> updateOrderStatus(@Valid @PathVariable Long userId, @Valid @PathVariable Long orderId) {
         orderService.updateOrderStatus(userId, orderId);
--- src/main/java/com/swiggy/order/controller/UserController.java
+++ src/main/java/com/swiggy/order/controller/UserController.java
@@ -2,6 +2,13 @@ package com.swiggy.order.controller;
 
 import com.swiggy.order.dto.UserDto;
 import com.swiggy.order.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,11 +20,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "UserController", description = "APIs for managing users")
 @RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "Create a new user",
+            description = "Create a new user")
+    @Parameter(name = "userRequestDto", description = "The DTO containing information for creating a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Successfully created a new user",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDto.class))}),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid input for creating a user")
+    })
     @PostMapping
     public ResponseEntity<String> registerClient(@Valid @RequestBody UserDto userDto) {
         userService.register(userDto);
